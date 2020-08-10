6ac54af32400288c273400ff2ed2d4f265fe6342
Valentin Paes <valentin.paes0@gmail.com>
2020-08-10 20:03:23 -0300

    :wrench: Consertando config do CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-    before_action :cors_preflight_checker
-    after_filter :cors_set_access_control_headers
+    before_action :cors_preflight_check
+    after_action :cors_set_access_control_headers
 
     protected 
 
@@ -8,19 +8,19 @@ class ApplicationController < ActionController::API
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'ṔOST, DELETE, GET, PUT, PATCH, OPTIONS'
         headers['Access-Control-Allow-Headers'] = '*'
-        headers['Access-Control-Allow-Max-Age'] = '1728000'
+        headers['Access-Control-Max-Age'] = '1728000'
     end 
 
     # If this is a preflight OPTIONS request, then short-circuit the
     # request, return only the necessary headers and return an empty
     # text/plain
 
-    def cors_prelight_check 
+    def cors_preflight_check 
         if request.method == :options
             headers['Access-Control-Allow-Origin'] = '*'
             headers['Access-Control-Allow-Methods'] = 'ṔOST, DELETE, GET, PUT, PATCH, OPTIONS'
             headers['Access-Control-Allow-Headers'] = '*'
-            headers['Access-Control-Allow-Max-Age'] = '1728000'
+            headers['Access-Control-Max-Age'] = '1728000'
             render :text => '', :content_type => 'text/plain'
         end 
     end
