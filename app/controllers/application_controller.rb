889e6c0caa953d3e0b15187c679d9c850ac2af50
Valentin Paes <valentin.paes0@gmail.com>
2020-08-18 16:10:45 -0300

    🔧 Config cors via Rack::Cors gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,3 @@
 class ApplicationController < ActionController::API
-    before_action :cors_preflight_check
-    after_action :cors_set_access_control_headers
 
-    protected 
-
-    def cors_set_access_control_headers 
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'ṔOST, DELETE, GET, PUT, PATCH, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = '*'
-        headers['Access-Control-Max-Age'] = '1728000'
-    end 
-
-    # If this is a preflight OPTIONS request, then short-circuit the
-    # request, return only the necessary headers and return an empty
-    # text/plain
-
-    def cors_preflight_check 
-        if request.method == :options
-            headers['Access-Control-Allow-Origin'] = '*'
-            headers['Access-Control-Allow-Methods'] = 'ṔOST, DELETE, GET, PUT, PATCH, OPTIONS'
-            headers['Access-Control-Allow-Headers'] = '*'
-            headers['Access-Control-Max-Age'] = '1728000'
-            render :text => '', :content_type => 'text/plain'
-        end 
-    end
 end
