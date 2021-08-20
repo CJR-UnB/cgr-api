a444a6985b481a10136013a78f5527945e63f27e
Valentin <valentin.paes0@gmail.com>
2021-08-20 13:55:35 -0300

    ✅ Remove necessidade do usuário ter um nome

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe User, type: :model do
 			expect(user).to be_valid
 		end
 
-		it "is not valid if the name is blank" do
-			new_user = build(:user, email: "popao@gmail.com", name: nil)
-			expect(new_user).to_not be_valid
-		end
+		# it "is not valid if the name is blank" do
+		# 	new_user = build(:user, email: "popao@gmail.com", name: nil)
+		# 	expect(new_user).to_not be_valid
+		# end
 
 		it "is not valid if the email has already been taken" do 
 			new_user = build(:user, name: "Valentin Ferreira Paes")
