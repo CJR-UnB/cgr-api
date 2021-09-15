eb4ecd96ee66ed27875b23c371a36af6e0aeffa6
Valentin <valentin.paes0@gmail.com>
2021-09-15 13:30:23 -0300

    ✅ Refatoração dos testes do controller de Members

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -1,39 +1,95 @@
 require "rails_helper"
 
 RSpec.describe "MembersController", :type => :request do
-    
-    let(:team) { create(:team)}
-    let(:role) { create(:role) }
-    
-    let(:team2) { create(:team, name: "Núcleo de Atendimento e Vendas", initials: "NAV")}
-    let(:role2) {create(:role, name: "Líder de Atendimento e Vendas", team: team2)}
-    let(:member) { create(:member, name: "Larissa Santana de Freitas Andrade", roles: [role2])}
 
-    it "creates a Member" do
-        headers = { "ACCEPT" => "application/json" }
-        new_member = { :name => "Valentin Ferreira Paes" }
-        post "/members", :params => { :member => new_member }, :headers => headers
-        body = JSON.parse(response.body)
+    before(:all) { @members_path = "/members"}
+
+    # GET /members
+    describe "#index" do
+        before(:context) do 
+            get @members_path 
+        end 
         
-        expect(response.content_type).to match(/application\/json/)
-        expect(response).to have_http_status(:created)
-        expect(body['name']).to eq(new_member[:name])
-    end
+        describe "response" do 
+            it "returns a JSON" do 
+                expect(response.content_type).to match(/application\/json/)
+            end 
 
-    it "shows all Members information" do 
-        get "/members"
-
-        expect(response.content_type).to match(/application\/json/)
-        expect(response).to have_http_status(:ok)
+            it "returns HTTP status 200 (OK)" do 
+                expect(response).to have_http_status(:ok)
+            end
+        end 
     end 
 
-    it "shows a Member's information" do 
-        get "/members/#{member.id}"
-        body = JSON.parse(response.body)
+    # POST /members 
+    describe "#create" do 
+        before(:context) do
+            @headers = { "ACCEPT" => "application/json" }
+            @new_member = { :name => "Valentin Ferreira Paes" }
+            post @members_path, :params => { :member => @new_member }, :headers => @headers
+            @body = JSON.parse(response.body)
+        end
 
-        expect(response.content_type).to match(/application\/json/)
-        expect(response).to have_http_status(:ok)
-        expect(body['name']).to eq(member.name)
+        after(:context) do
+            Member.find_by(name: @new_member[:name]).destroy 
+        end 
+
+        describe "response" do
+            it "returns a JSON" do
+                expect(response.content_type).to match(/application\/json/)
+            end 
+
+            it "returns HTTP status 201 (Created)" do
+                expect(response).to have_http_status(:created)
+            end
+            
+            it "returns a Member matching the params given" do
+                expect(@body['name']).to eq(@new_member[:name])
+            end
+
+            it "creates a new Member" do
+                expect{ 
+                    post @members_path, :params => { :member => { :name => "Dapedo"}}, :headers => @headers
+                }.to change(Member, :count).by(1)
+            end
+        end
+    end
+
+    context "existing single Member actions" do 
+        before(:context) do 
+            @role = create(:role)
+            @team = @role.team
+            @member = create(:member, roles: [@role])
+        end
+
+        after(:context) do
+            MemberRole.delete_all
+            Role.delete_all
+            Team.delete_all
+            Member.delete_all
+        end 
+
+        # GET /members/:id
+        describe "#show" do 
+            before(:context) do 
+                get (@members_path + "/#{@member.id}")
+                @body = JSON.parse(response.body)
+            end 
+
+            describe "response" do
+                it "returns a JSON" do 
+                    expect(response.content_type).to match(/application\/json/)
+                end
+
+                it "returns a HTTP status 200 (OK)" do 
+                    expect(response).to have_http_status(:ok)
+                end 
+
+                it "returns a JSON matching the member's attributes" do
+                    expect(@body['name']).to eq(@member.name)
+                end
+            end 
+        end
     end 
 
     # it "updates a Member info" do
